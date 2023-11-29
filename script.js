67846769ec5c51d9732ec8e9e96b87f58d70eda4
Ernestasviduolis <eviduolis88@gmail.com>
2023-11-29 18:06:11 +0200

    papildymas1

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
 const menuToggle = document.getElementById('menuToggle');
 const mobileMenu = document.querySelector('.bemeniu');
+const CloseMenu =  document.querySelector('.open');
+
 
 menuToggle.addEventListener('click', () => {
     mobileMenu.classList.toggle('show');
+    CloseMenu.classList.toggle("close");
+
 });
+
+
+
 });
